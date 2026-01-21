fdf40e20fe00cbe85f36e0b36b68b56e46c22655
24DP3KGodi <A242608KG@rvt.lv>
2026-01-21 11:09:57 +0200

    Person and subclasses

---
+++ src/main/java/rvt/Student.java
@@ -0,0 +1,24 @@
+package rvt;
+
+public class Student extends Person {
+    private int credits;
+
+    public Student(String name, String address) {
+        super(name, address);
+        this.credits = 0;
+    }
+
+    public int getCredits() {
+        return this.credits;
+    }
+
+    public void study() {
+        this.credits += 1;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (" + getAddress() + ") has " + this.credits + " credits";
+}
+}
+   
---
+++ src/main/java/rvt/Teacher.java
@@ -0,0 +1,20 @@
+package rvt;
+
+public class Teacher extends Person {
+    private int salary;
+
+    public Teacher(String name, String address, int salary) {
+        super(name, address);
+        this.salary = salary;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (" + getAddress() + ") earns " + this.salary + " euros";
+    }
+  
+}
